4d2dc780e58cc9e63b8008de90e930ef89c0d21d
natan <natansheena@gmail.com>
2022-02-27 00:44:41 +0200

    Added tooltip instead of popup

--- client/src/views/HomePage/HomePage.tsx
+++ client/src/views/HomePage/HomePage.tsx
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client';
+import CowsTrackerMap from './Map/CowsTrackerMap';
+import { GpsEvent } from '../../common/interfaces/gps-event.interface';
 
-// Import components
-import Map from './Map/Map';
-
-const socket = io('localhost:3344/api')
+// TODO Save domain
+const socket = io('http://127.0.0.1:3344/', { transports : ['websocket'] })
 socket.connect()
 
 function HomePage() {
+  const [gpsEvents, setGpsEvents] = useState<{ [cowId: string]: GpsEvent }>({
+    '002': {
+      cowId: "002",
+      timestamp: "2022-03-05T23:49:22",
+      latLong: "31.778345, 35.225079",
+      wt: "3S",
+      battery: "399V",
+      counter: 23
+    },
+    '001': {
+      cowId: "001",
+      timestamp: "2022-03-05T23:49:22",
+      latLong: "31.788345, 35.225079",
+      wt: "3S",
+      battery: "399V",
+      counter: 23
+    },
+  });
+
+  const startListenToGpsEvents = () => {
+    socket.on('gps-event', (event: GpsEvent) => {
+      setGpsEvents({ ...gpsEvents, [event.cowId]: event })
+    })
+  }
+
   useEffect(() => {
-    console.log(44)
+    startListenToGpsEvents();
   }, [])
 
   return (
     <>
-      <Map />
+      <CowsTrackerMap 
+        gpsEvents={gpsEvents}
+      />
     </>
   )
 };
---
+++ client/src/views/HomePage/Map/CowsTrackerMap.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
+import L, { LatLngExpression } from 'leaflet';
+import Typography from '@mui/material/Typography';
+import 'leaflet/dist/leaflet.css';
+import icon from '../../../assets/cow.png';
+import { GpsEvent } from '../../../common/interfaces/gps-event.interface';
+
+let CowIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [24, 30],
+  iconAnchor: [7, 10]
+});
+
+interface Props {
+  gpsEvents: { [cowId: string]: GpsEvent },
+}
+
+function CowsTrackerMap(props: Props) {
+  const { gpsEvents } = props;
+  const [center, setCenter] = useState<LatLngExpression>([31.778345, 35.225079]);
+  
+  return (
+    <>
+      <MapContainer style={{ height: "100vh", width: "100%" }} center={center} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {
+          Object.values(gpsEvents).map((event, key) => {
+            return (
+              <>
+              {
+                event.latLong && 
+                  <>
+                    <Marker 
+                      key={key} 
+                      icon={ CowIcon } 
+                      position={event.latLong.split(',').map(tt => +tt) as LatLngExpression}
+                      riseOnHover={true}
+                    >
+                      <Tooltip
+                        permanent
+                        key={key}
+                      >
+                        <Typography fontWeight="bold" variant="caption" display="block" gutterBottom>
+                          {event.cowId} | {event.timestamp}
+                        </Typography>
+                      </Tooltip>
+                    </Marker>
+                  </>
+              }
+              </>
+            )
+          })
+        }
+      </MapContainer>
+    </>
+  )
+
+}
+
+export default CowsTrackerMap;
